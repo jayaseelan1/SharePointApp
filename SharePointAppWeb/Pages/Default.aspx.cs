45c8352aa2720d22c0c0942e2b586dc244d9d535
jayaseelan1 <jlurdusami@worldbankgroup.org>
2015-12-23 06:14:07 -0500

    Branch 2 changed

--- SharePointAppWeb/Pages/Default.aspx.cs
+++ SharePointAppWeb/Pages/Default.aspx.cs
@@ -31,13 +31,14 @@ namespace SharePointAppWeb
             // The following code gets the client context and Title property by using TokenHelper.
             // To access other properties, the app may need to request permissions on the host web.
             var spContext = SharePointContextProvider.Current.GetSharePointContext(Context);
-
             using (var clientContext = spContext.CreateUserClientContextForSPHost())
             {
                 clientContext.Load(clientContext.Web, web => web.Title);
                 clientContext.ExecuteQuery();
                 Response.Write(clientContext.Web.Title);
             }
+            //Added in branch 2 by jayaseelan
+            Response.Write("Welcome");
         }
     }
 }
